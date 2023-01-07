f144bb5e034e097ca8974632c3990e2799d17143
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-07 20:44:18 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Denny Arfansyah on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
